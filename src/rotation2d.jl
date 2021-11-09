67df811485b68593406a77a94619ee7df7c74988
isaac553876299 <60979696+isaac553876299@users.noreply.github.com>
2021-11-09 19:34:06 +0100

    h

---
+++ src/rotation2d.jl
@@ -0,0 +1,15 @@
+# les columnes d'una matriu són les imatges dels vectors de la base
+[cos -sin; sin cos][x; y] # independent del quadrant
+
+# números
+# N naturals 1
+# Z enters -1
+# Q racionals 1/2
+# R reals 2^(1/2)
+# C complexos 2^(-1/2)
+# H quaternions a+ib+jc+kd
+# O octonions...8...16.
+
+# greatest 8 symbol natural number
+9^99999!
+#
