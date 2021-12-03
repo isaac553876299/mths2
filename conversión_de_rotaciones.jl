15977070683466a28c33889402d2c4f2d0cff4a3
isaacdd <isaacdd.bcn.2001@gmail.com>
2021-12-04 00:02:52 +0100

    h

--- conversión_de_rotaciones.jl
+++ conversión_de_rotaciones.jl
@@ -3,22 +3,21 @@
 using LinearAlgebra
 
 function axis_angle_to_mat(u,θ)
-    u=[u[1];u[2];u[3]]
     R = cos(θ)*I + (1-cos(θ))*(u*u') + sin(θ)*[0 -u[3] u[2]; u[3] 0 -u[1]; -u[2] u[1] 0]
     return R
 end
 
 function axis_angle_to_quat(u,θ)
-    q = [sin(θ/2)*u'; cos(θ/2)]
+    q = [sin(θ/2)*u; cos(θ/2)]
     return q
-end#
+end
 
 function mat_to_axis_angle(m)
     θ = acos((tr(m)-1)/2)
     sm=[m[3,2]-m[2,3]; m[1,3]-m[3,1]; m[2,1]-m[1,2]]
     u=sm/norm(sm)
     return u,θ
-end#
+end
 
 function mat_to_quat(m)
     m00 = m[1, 1]
@@ -30,7 +29,7 @@ function mat_to_quat(m)
     q[3] = sqrt(max(0, 1-m00-m11+m22))/2;
     q[4] = sqrt(max(0, 1+m00+m11+m22))/2;
     return q
-end#
+end
 
 function quat_to_axis_angle(q)
     qw = q[4]
@@ -38,7 +37,7 @@ function quat_to_axis_angle(q)
     b = sqrt(1-qw^2)
     u = (0.0001 > b) ? [1 0 0] : [q[1]/b q[2]/b q[3]/b]
     return u,θ
-end#
+end
 
 function quat_to_mat(q)
     q1 = q[1]
@@ -53,12 +52,12 @@ function quat_to_mat(q)
     r3 = 2 * [(q1*q3-q4*q2), (q2*q3+q4*q1), ((q4^2)+(q3^2))]
     r3[3] -= 1
 
-    R = [r1 r2 r3]
+    R = [r1 r2 r3]'
     return R
-end#
+end
 
 
-test_u = [0.2672612 0.5345225 0.8017837]
+test_u = [0.2672612; 0.5345225; 0.8017837]
 test_θ = 1.5040802
 test_m = [0.1333333 -0.6666667 0.7333333; 0.9333333 0.3333333 0.1333333; -0.3333333 0.6666667 0.6666667]
 test_q = [0.1825742 0.3651484 0.5477226 0.7302967]
