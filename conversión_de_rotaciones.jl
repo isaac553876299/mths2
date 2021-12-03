e15f0c77c5b0c0aaa30ec81f9bf6494bb5fedd24
isaac553876299 <60979696+isaac553876299@users.noreply.github.com>
2021-12-03 09:59:12 +0100

    f

--- conversión_de_rotaciones.jl
+++ conversión_de_rotaciones.jl
@@ -1,21 +1,25 @@
 
 
 function axis_angle_to_mat(θ,u)
-    return #= R= =#I+sin(θ)*u+(1-cos(θ))*(u^2)
+    R=I+sin(θ)*u+(1-cos(θ))*(u^2)
     #return #= R= =#(1-cos(θ))*I+sin(θ)*u+(1-cos(θ))*((u^2)+I)
     #Rodrigues'?
     #cos(θ)*I+sin(θ)*u+(1-cos(θ))*(uXu)
+    cos(θ)*I+(1-cos(θ))*(uxu)+sin(θ)*[0 -u[3] u[2]; u[3] 0 -u[1]; -u[2] u[1] 0]
+    return R
 end
 
 
 function axis_angle_to_quat(θ,u)
-    return #= q= =#[cos(θ);sin(θ)*u]
+    q=[cos(θ);sin(θ)*u]
+    return q
 end
 
 
 function mat_to_axis_angle(m)
     θ=acos((tr(m)-1)/2)
     u=((m-m')/2)*sin(θ)
+    return θ,u
 end
 
 
@@ -23,21 +27,23 @@ function mat_to_quat(m)
     m00 = m[0][0]
     m11 = m[1][1]
     m22 = m[2][2]
-    absQ2 = det(m)^(1/3)
+    #absQ2 = det(m)^(1/3)
     #https://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/christian.htm
     q.w = sqrt(max(0, 1 + m00 + m11 + m22)) / 2;
     q.x = sqrt(max(0, 1 + m00 - m11 - m22)) / 2;
     q.y = sqrt(max(0, 1 - m00 + m11 - m22)) / 2;
     q.z = sqrt(max(0, 1 - m00 - m11 + m22)) / 2;
+    return q
 end
 
 
 function quat_to_axis_angle(q)
     qw=q.w
     θ = 2 * acos(q.w)
-    u.x = q.x / sqrt(1-qw^2)
-    u.y = q.y / sqrt(1-qw^2)
-    u.z = q.z / sqrt(1-qw^2)
+    u.x = q.x / sqrt(1 - qw^2)
+    u.y = q.y / sqrt(1 - qw^2)
+    u.z = q.z / sqrt(1 - qw^2)
+    return θ,u
 end
 
 
@@ -55,18 +61,6 @@ function quat_to_mat(q)
     r3[3] -= 1
 
     R = [r1, r2, r3]
-
-    r00 = 2 * (q0^2 + q1^2) - 1
-    r01 = 2 * (q1 * q2 - q0 * q3)
-    r02 = 2 * (q1 * q3 + q0 * q2)
-
-    r10 = 2 * (q1 * q2 + q0 * q3)
-    r11 = 2 * (q0^2 + q2^2) - 1
-    r12 = 2 * (q2 * q3 - q0 * q1)
-
-    r20 = 2 * (q1 * q3 - q0 * q2)
-    r21 = 2 * (q2 * q3 + q0 * q1)
-    r22 = 2 * (q0^2 + q3^2) - 1
 
     R = [[r00, r01, r02], [r10, r11, r12], [r20, r21, r22]]
 end
